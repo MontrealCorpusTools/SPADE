1184272852fd159f8d15c4e9aafff244351f9a58
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2017-10-18 15:47:17 -0400

    Further update

--- basic_queries.py
+++ basic_queries.py
@@ -1,23 +1,9 @@
 import sys
 import os
+import argparse
 
-# =============== USER CONFIGURATION ===============
-corpus_name = "raleigh"
-corpus_dir = "/mnt/e/temp/raleigh/smallest_raleigh"
-textgrid_format = "MFA"
-vowel_inventory = []
 base_dir = os.path.dirname(os.path.abspath(__file__))
-script_dir = os.path.join(os.path.dirname(base_dir), 'Common')
-reset = False  # Setting to True will cause the corpus to re-import
-
-# SPADE CONFIG
-
-dialect_code = 'sca'
-
-lexicon_enrichment_files = ['/mnt/e/temp/raleigh/rule_applications.csv', '/mnt/e/temp/raleigh/sca_comparison.csv']
-speaker_enrichment_file = '/mnt/e/temp/raleigh/speaker_information.txt'
-
-# ==================================================
+script_dir = os.path.join(base_dir, 'Common')
 
 sys.path.insert(0, script_dir)
 
@@ -30,6 +16,22 @@ from polyglotdb.utils import ensure_local_database_running
 from polyglotdb import CorpusConfig
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('corpus_name', help='Name of the corpus')
+    parser.add_argument('-r', '--reset', help="Reset the corpus", action='store_true')
+
+    args = parser.parse_args()
+    corpus_name = args.corpus_name
+    reset = args.reset
+    directories = [x for x in os.listdir(base_dir) if os.path.isdir(x) and x != 'Common']
+
+    if args.corpus_name not in directories:
+        print(
+            'The corpus {0} does not have a directory (available: {1}).  Please make it with a {0}.yaml file inside.'.format(
+                args.corpus_name, ', '.join(directories)))
+        sys.exit(1)
+    print('Processing...')
+    corpus_conf = common.load_config(corpus_name)
     print('Processing...')
     with ensure_local_database_running(corpus_name) as params:
         print(params)
@@ -38,11 +40,11 @@ if __name__ == '__main__':
         # Common set up
         if reset:
             common.reset(config)
-        common.loading(config, corpus_dir, textgrid_format)
-        common.basic_enrichment(config, vowel_inventory)
+        common.loading(config, corpus_conf['corpus_directory'], corpus_conf['input_format'])
+        common.basic_enrichment(config, corpus_conf['vowel_inventory'])
 
-        common.lexicon_enrichment(config, lexicon_enrichment_files, dialect_code)
-        common.speaker_enrichment(config, speaker_enrichment_file)
+        common.lexicon_enrichment(config, corpus_conf['unisyn_spade_directory'], corpus_conf['dialect_code'])
+        common.speaker_enrichment(config, corpus_conf['speaker_enrichment_file'])
 
         common.basic_queries(config)
 
--- formant.py
+++ formant.py
@@ -1,44 +1,34 @@
 import sys
 import os
+import argparse
 
-# =============== USER CONFIGURATION ===============
-corpus_name = "raleigh"
-corpus_dir = "/mnt/e/temp/raleigh/small_raleigh"
-textgrid_format = "MFA"
-vowel_inventory = ['ER0', 'IH2', 'EH1', 'AE0', 'UH1', 'AY2', 'AW2', 'UW1', 'OY2', 'OY1', 'AO0', 'AH2', 'ER1', 'AW1',
-                   'OW0', 'IY1', 'IY2', 'UW0', 'AA1', 'EY0', 'AE1', 'AA0', 'OW1', 'AW0', 'AO1', 'AO2', 'IH0', 'ER2',
-                   'UW2', 'IY0', 'AE2', 'AH0', 'AH1', 'UH2', 'EH2', 'UH0', 'EY1', 'AY0', 'AY1', 'EH0', 'EY2', 'AA2',
-                   'OW2', 'IH1']
 base_dir = os.path.dirname(os.path.abspath(__file__))
-script_dir = os.path.join(os.path.dirname(base_dir), 'Common')
-reset = False  # Setting to True will cause the corpus to re-import
-
-# SPADE CONFIG
-
-dialect_code = 'sca'
-
-lexicon_enrichment_files = ['/mnt/e/temp/raleigh/rule_applications.csv', '/mnt/e/temp/raleigh/sca_comparison.csv']
-speaker_enrichment_file = '/mnt/e/temp/raleigh/speaker_information.txt'
-
-# FORMANT CONFIG
-
-stressed_vowels = [x for x in vowel_inventory if x.endswith('1')]
-csv_path = os.path.join(base_dir, corpus_name + "_formants.csv")
-
-# ==================================================
+script_dir = os.path.join(base_dir, 'Common')
 
 sys.path.insert(0, script_dir)
 
 import common
 
-import re
-import time
-
 from polyglotdb.utils import ensure_local_database_running
 from polyglotdb import CorpusConfig
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('corpus_name', help='Name of the corpus')
+    parser.add_argument('-r', '--reset', help="Reset the corpus", action='store_true')
+
+    args = parser.parse_args()
+    corpus_name = args.corpus_name
+    reset = args.reset
+    directories = [x for x in os.listdir(base_dir) if os.path.isdir(x) and x != 'Common']
+
+    if args.corpus_name not in directories:
+        print(
+            'The corpus {0} does not have a directory (available: {1}).  Please make it with a {0}.yaml file inside.'.format(
+                args.corpus_name, ', '.join(directories)))
+        sys.exit(1)
     print('Processing...')
+    corpus_conf = common.load_config(corpus_name)
     with ensure_local_database_running(corpus_name) as params:
         print(params)
         config = CorpusConfig(corpus_name, **params)
@@ -46,13 +36,14 @@ if __name__ == '__main__':
         # Common set up
         if reset:
             common.reset(config)
-        common.loading(config, corpus_dir, textgrid_format)
-        common.basic_enrichment(config, vowel_inventory)
+        common.loading(config, corpus_conf['corpus_directory'], corpus_conf['input_format'])
+        common.basic_enrichment(config, corpus_conf['vowel_inventory'])
 
-        common.lexicon_enrichment(config, lexicon_enrichment_files, dialect_code)
-        common.speaker_enrichment(config, speaker_enrichment_file)
+        common.lexicon_enrichment(config, corpus_conf['unisyn_spade_directory'], corpus_conf['dialect_code'])
+        common.speaker_enrichment(config, corpus_conf['speaker_enrichment_file'])
 
         # Formant specific analysis
-        common.formant_acoustic_analysis(config, stressed_vowels)
-        common.formant_export(config, stressed_vowels, csv_path, dialect_code)
+        common.formant_acoustic_analysis(config, corpus_conf['stressed_vowels'])
+        common.formant_export(config, corpus_conf['stressed_vowels'], corpus_name, corpus_conf['dialect_code'],
+                              corpus_conf['speakers'])
         print('Finishing up!')
