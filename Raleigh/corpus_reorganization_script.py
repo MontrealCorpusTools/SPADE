7785e32eeb00906ce53f33b0b7ec2f287c56468b
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2017-10-24 16:23:10 -0400

    Add Raleigh reorganization script and stub readme

--- Raleigh/corpus_reorganization_script.py
+++ Raleigh/corpus_reorganization_script.py
@@ -10,7 +10,7 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 speaker_path = os.path.join(base_dir, 'raleigh_files_sub.csv')
 small_raleigh_dir = os.path.join(os.path.dirname(base_dir), 'Raleigh')
 original_tg_dir = os.path.join(base_dir, 'long_textgrids')
-#shutil.rmtree(small_raleigh_dir, ignore_errors=True)
+# shutil.rmtree(small_raleigh_dir, ignore_errors=True)
 wav_dir = os.path.join(base_dir, 'textgrid-wav')
 os.makedirs(small_raleigh_dir, exist_ok=True)
 selected_speaker_path = os.path.join(small_raleigh_dir, 'speaker_data.csv')
@@ -18,7 +18,7 @@ selected_speaker_path = os.path.join(small_raleigh_dir, 'speaker_data.csv')
 speaker_data = {}
 
 selected_speakers = []
-selected_counts = {('old','male'):0, ('old','female'):0, ('young','male'):0, ('young','female'):0}
+selected_counts = {('old', 'male'): 0, ('old', 'female'): 0, ('young', 'male'): 0, ('young', 'female'): 0}
 
 num_speakers = 20
 
@@ -27,7 +27,7 @@ median_age = 1957
 with open(speaker_path, 'r') as f:
     reader = csv.DictReader(f)
     for line in reader:
-        speaker_data[line['speaker']] = {'birthyear': int(line['birthyear']), 'sex':line['sex']}
+        speaker_data[line['speaker']] = {'birthyear': int(line['birthyear']), 'sex': line['sex']}
 
 speakers = list(speaker_data.keys())
 random.shuffle(speakers)
@@ -36,7 +36,7 @@ for s in speakers:
     if speaker_data[s]['birthyear'] > 1957:
         age = 'young'
     sex = speaker_data[s]['sex']
-    #if selected_counts[(age, sex)] > num_speakers/4 - 1:
+    # if selected_counts[(age, sex)] > num_speakers/4 - 1:
     #    continue
     selected_counts[(age, sex)] += 1
     selected_speakers.append(s)
@@ -75,7 +75,7 @@ with open(selected_speaker_path, 'w') as f:
                 elif tier.name == 'S1word':
                     tier.name = s + '1 - word'
                 elif tier.name == 'Iphone':
-                    tier.name = 'Interviewer'+ s + ' - ' + 'phone'
+                    tier.name = 'Interviewer' + s + ' - ' + 'phone'
                 elif tier.name == 'Iword':
                     tier.name = 'Interviewer' + s + ' - ' + 'word'
                 else:
@@ -84,9 +84,7 @@ with open(selected_speaker_path, 'w') as f:
             if not new_tg.tiers:
                 continue
             os.makedirs(os.path.join(small_raleigh_dir, s), exist_ok=True)
-            new_tg.write(os.path.join(small_raleigh_dir,s, f))
-            new_wav_path = os.path.join(small_raleigh_dir,s, wav_f)
+            new_tg.write(os.path.join(small_raleigh_dir, s, f))
+            new_wav_path = os.path.join(small_raleigh_dir, s, wav_f)
             if not os.path.exists(new_wav_path):
                 subprocess.call(['sox', wav_path, new_wav_path, 'rate', '-I', str(22500)])
-
-
