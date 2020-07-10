1830032ab0529e52a0ac1e1f6ec52688cb07509f
James Tanner <james.tanner@mail.mcgill.ca>
2020-07-10 10:40:19 -0400

    Update rhotics script with columns and comments

--- rhotics.py
+++ rhotics.py
@@ -1,3 +1,18 @@
+###################################
+## SPADE rhotics analysis script ##
+###################################
+
+## Processes and analyses linguistic and acoustic properties of
+## word-final rhotics from corpora collected as part of The
+## SPeech Across Dialects of English (SPADE) project
+
+## Input:
+## - corpus name (e.g., spade-Buckeye)
+## - corpus metadata (in YAML file within the corpus directory
+
+## Output:
+## - CSV of measures for word-final rhotic segments
+
 import sys
 import os
 import argparse
@@ -19,6 +34,16 @@ from polyglotdb import CorpusConfig
 
 def rhotics_export(config, corpus_name, corpus_directory, dialect_code, speakers, vowel_prototypes_path, reset_formants, ignored_speakers=None):
     csv_path = os.path.join(base_dir, corpus_name, '{}_rhotics.csv'.format(corpus_name))
+    '''
+    Main function for processing rhotics. Takes in corpus information and returns
+    formant tracks, in the form of 21 equally-spaced points throughout the token
+    (i.e. 5% intervals), meaning that 21 rows (observations) are returned for
+    each rhotic token.
+
+    The presence of rhotics is determined through corpus phone labels ('r') and
+    UNISYN labels ('rhotics') associated with a token; be default, this function
+    analyses only word-final rhotics in both monosyllabic and polysyllabic words.
+    '''
 
     # list of unisyn vowels included in formant analysis
     rhotics = ['ar', 'or', 'our', '@r', 'aer', 'oir', 'owr', 'ir', 'er', 'eir', 'ur']
@@ -27,6 +52,10 @@ def rhotics_export(config, corpus_name, corpus_directory, dialect_code, speakers
     beg = time.time()
 
     with CorpusContext(config) as c:
+
+        ## define the subset of tokens to be measured by formant tracking:
+        ## these are rhotic tokens (determined by corpus and UNISYN labels)
+        ## which are word-final and at least 50ms in duration
         if c.hierarchy.has_type_property('word', 'unisynprimstressedvowel1'):
             q = c.query_graph(c.phone)
             q = q.filter(c.phone.syllable.word.unisynprimstressedvowel1.in_(rhotics))
@@ -42,14 +71,24 @@ def rhotics_export(config, corpus_name, corpus_directory, dialect_code, speakers
             print('{} has not been enriched with Unisyn information.'.format(corpus_name))
             return
 
+        ## reset the formant measures by using the -f flag:
+        ## this is useful for experimenting with different
+        ## formant tracking settings without needing to
+        ## re-import the entire corpus
         if reset_formants:
             print("Resetting formants")
             c.reset_acoustics()
             print('Beginning formant calculation')
 
+    ## perform formant tracking
+    ## see common.py for how this is implemented
     common.formant_acoustic_analysis(config, None, vowel_prototypes_path, drop_formant = drop_formant, output_tracks = True, subset="unisyn_subset")
 
     with CorpusContext(config) as c:
+        ## since the subsetting for measuring tokens
+        ## and extracting them for output are separate,
+        ## it is necessary to re-declare the subset of
+        ## tokens to be included in the output
         print('Beginning formant export')
         q = c.query_graph(c.phone)
         q = q.filter(c.phone.subset == 'unisyn_subset')
@@ -60,11 +99,19 @@ def rhotics_export(config, corpus_name, corpus_directory, dialect_code, speakers
             q = q.filter(c.phone.speaker.name.in_(speakers))
         q = q.filter(c.phone.duration >= 0.05)
         print('Applied filters')
+
+        ## define the properties of the formant track
+        ## to be exported
         formants_prop = c.phone.formants
         formants_prop.relative_time = True
-        #formants_track = formants_prop.track
         formants_track = formants_prop.interpolated_track
         formants_track.num_points = 21
+
+        ## define columns to be included in the output file
+        ## these include information about the phone (segment
+        ## label, start/end etc), surrounding segments, the
+        ## syllable, the word, speaker properties, as well
+        ## as acoustic information as speech rate
         q = q.columns(c.phone.speaker.name.column_name('speaker'),
                       c.phone.discourse.name.column_name('discourse'),
                       c.phone.id.column_name('phone_id'),
@@ -82,12 +129,26 @@ def rhotics_export(config, corpus_name, corpus_directory, dialect_code, speakers
                       c.phone.utterance.speech_rate.column_name('speech_rate'),
                       c.phone.syllable.label.column_name('syllable_label'),
                       c.phone.syllable.duration.column_name('syllable_duration'),
+                      c.phone.syllable.begin.column_name('syllable_begin'),
+                      c.phone.syllable.end.column_name('syllable_end'),
+                      c.phone.word.duration.column_name('word_duration'),
+                      c.phone.word.begin.column_name('word_begin'),
+                      c.phone.word.end.column_name('word_end'),
+                      c.phone.utterance.duration.column_name('utterance_duration'),
+                      c.phone.utterance.begin.column_name('utterance_begin'),
+                      c.phone.utterance.end.column_name('utterance_end'),
+                      c.phone.word.transcription.column_name('word_transcription'),
+                      c.phone.word.num_syllables.column_name('word_num_syllables'),
+                      c.phone.utterance.num_syllables.column_name('utterance_num_syllables'),
+                      c.phone.utterance.num_words.column_name('utterance_num_words'),
                       formants_track)
         for sp, _ in c.hierarchy.speaker_properties:
             if sp == 'name':
                 continue
             q = q.columns(getattr(c.phone.speaker, sp).column_name(sp))
 
+        ## Since the Buckeye corpus uses phonetic transcriptions by default,
+        ## also extract the underlying (phonological) segment labels
         if c.hierarchy.has_token_property('word', 'surface_transcription'):
             print('getting underlying and surface transcriptions')
             q = q.columns(
@@ -102,7 +163,7 @@ def rhotics_export(config, corpus_name, corpus_directory, dialect_code, speakers
         print("Results for query written to {}".format(csv_path))
         common.save_performance_benchmark(config, 'rhotics_export', time_taken)
 
-
+## parse command-line arguments
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('corpus_name', help='Name of the corpus')
@@ -117,6 +178,7 @@ if __name__ == '__main__':
     reset_formants = args.formant_reset
     directories = [x for x in os.listdir(base_dir) if os.path.isdir(x) and x != 'Common']
 
+    ## check the corpus has a metadata file
     if args.corpus_name not in directories:
         print(
             'The corpus {0} does not have a directory (available: {1}).  Please make it with a {0}.yaml file inside.'.format(
@@ -126,8 +188,12 @@ if __name__ == '__main__':
     print('Processing...')
 
     ignored_speakers = corpus_conf.get('ignore_speakers', [])
+    ## if the -r flag is used, the database information for the
+    ## corpus will be deleted before the execution of the script
     if reset:
         common.reset(corpus_name)
+
+    ## check status of the ISCAN server
     ip = common.server_ip
     if docker:
         ip = common.docker_ip
@@ -135,20 +201,24 @@ if __name__ == '__main__':
         print(params)
         config = CorpusConfig(corpus_name, **params)
         config.formant_source = 'praat'
-        # Common set up
-        
+       
+        ## add basic enrichments for the corpus, such as syllables, utterances,
+        ## lexical and speaker information
         common.loading(config, corpus_conf['corpus_directory'], corpus_conf['input_format'])
-
         common.lexicon_enrichment(config, corpus_conf['unisyn_spade_directory'], corpus_conf['dialect_code'])
         common.speaker_enrichment(config, corpus_conf['speaker_enrichment_file'])
-
         common.basic_enrichment(config, corpus_conf['vowel_inventory'] + corpus_conf['extra_syllabic_segments'], corpus_conf['pauses'])
 
-        #vowel_prototypes_path = os.path.join(base_dir, corpus_name, '{}_rhotic_prototypes.csv'.format(corpus_name))
+        ## check for the presence of vowel prototypes:
+        ## this is not actively used for detecting formants,
+        ## so this is skipped in measurement and speaker-level
+        ## means are calculated at measurement-time
         vowel_prototypes_path = corpus_conf.get('vowel_prototypes_path','')
         if not vowel_prototypes_path:
             vowel_prototypes_path = os.path.join(base_dir, corpus_name, '{}_prototypes.csv'.format(corpus_name))
 
+        ## call above-defined formant function
+        ## and write export CSV file
         rhotics_export(config, corpus_name, corpus_conf['corpus_directory'], corpus_conf['dialect_code'],
                               corpus_conf['speakers'], vowel_prototypes_path = vowel_prototypes_path, reset_formants = reset_formants, ignored_speakers=ignored_speakers)
         print('Finishing up!')
