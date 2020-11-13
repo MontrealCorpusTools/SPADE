513887e1410f153292cab5721f2875ec8238a9d2
James Tanner <james.tanner@mail.mcgill.ca>
2020-11-13 10:25:10 -0500

    Process command line flags

--- run_mts_measures.py
+++ run_mts_measures.py
@@ -12,19 +12,22 @@ parser.add_argument("SoundDir", help = "Path to the top level of sound files")
 parser.add_argument("OutputDir", help = "Path to write out CSVs")
 args = parser.parse_args()
 
+## skipped corpora
+skipped = []
+
 ## corpora with subdirs
-subdirs = ['spade_Buckeye', 'spade_dapp_EnglandLDS', 'spade_dapp_Ireland', 'spade_dapp_Scotland', 
-           'spade_dapp_ScotlandNE', 'spade_dapp_Wales', 'spade_DECTE', 'spade_Edinburgh',
-           'spade_GlasgowBiD', 'spade_Glaswasian', 'spade_ICE_Sco', 'spade_Irish', 'spade_IViE_Belfast',
-           'spade_IViE_Bradford', 'spade_IViE_Cambridge', 'spade_IViE_Cardiff', 'spade_IViE_Dublin',
-           'spade_IViE_Leeds', 'spade_IViE_London', 'spade_IViE_Newcastle', 'spade_PAC_Ayr',
-           'spade_PEBL', 'spade_PettyHarbour', 'spade_Raleigh', 'spade_SOTC', 'spade_TIMIT']
+subdirs = ['spade-Buckeye', 'spade-dapp-EnglandLDS', 'spade-dapp-Ireland', 'spade-dapp-Scotland', 
+           'spade-dapp_ScotlandNE', 'spade-dapp-Wales', 'spade-DECTE', 'spade-Edinburgh',
+           'spade-GlasgowBiD', 'spade-Glaswasian', 'spade-ICE_Sco', 'spade-Irish', 'spade-IViE-Belfast',
+           'spade-IViE-Bradford', 'spade-IViE-Cambridge', 'spade-IViE-Cardiff', 'spade-IViE-Dublin',
+           'spade-IViE-Leeds', 'spade-IViE-London', 'spade-IViE-Newcastle', 'spade-PAC-Ayr',
+           'spade-PEBL', 'spade-PettyHarbour', 'spade-Raleigh', 'spade-SOTC', 'spade-TIMIT']
 
 ## corpora with numbers as discourse name
-numbers = ["spade_WYRED"]
+numbers = ["spade-WYRED"]
 
 ## corpora that use speaker name instead of discourse
-speakers = ['spade_DECTE']
+speakers = ['spade-DECTE']
 
 def getCorpusName(file):
     """Extract corpus name from CSV file"""
@@ -32,38 +35,45 @@ def getCorpusName(file):
 
     return corpus
 
-def runMTS(corpus, file, d = False, n = False, s = False):
+def runMTS(corpus, file, flag = []):
     """Call R process to run mts script"""
 
     inPath = os.path.join(args.InputDir, file)
     SoundPath = os.path.join(args.SoundDir, corpus, "audio_and_transcripts")
 
-    if d:
-        subprocess.call(['Rscript', 'generate_mts_measures.r', '-d', inPath, SoundPath, args.OutputDir])
-    elif n:
-        subprocess.call(['Rscript', 'generate_mts_measures.r', '-n', inPath, SoundPath, args.OutputDir])
-    elif s:
-        subprocess.call(['Rscript', 'generate_mts_measures.r', '-s', inPath, SoundPath, args.OutputDir])
-    else:
-        subprocess.call(['Rscript', 'generate_mts_measures.r', inPath, SoundPath, args.OutputDir])
+    ## command line call
+    cmd = ['Rscript', 'generate_mts_measures.r', inPath, SoundPath, args.OutputDir]
 
-def processFile(file):
+    ## insert flags into command line call
+    if len(flag) > 0:
+        for i, fl in enumerate(flag):
+            cmd.insert(i + 2, fl)
+
+    ## make R call
+    print(cmd)
+    subprocess.call(cmd)
+
+def processFile(file, f = []):
     """Get corpus name and run MTS
        script with arguments"""
 
     if file.endswith("csv"):
-        print(file)
 
         corpus = getCorpusName(file)
 
+        if corpus in skipped:
+            return
+
         if corpus in subdirs:
-            runMTS(corpus, file, d = True)
-        elif corpus in numbers:
-            runMTS(corpus, file, n = True)
-        elif corpus in speakers:
-            runMTS(corpus, file, s = True)
-        else:
-            runMTS(corpus, file)
+            f.append('-d')
+
+        if corpus in numbers:
+            f.append('-n')
+
+        if corpus in speakers:
+            f.append('-s')
+
+        runMTS(corpus, file, flag = f)
 
 ## run over directory
 list(map(processFile, os.listdir(args.InputDir)))
